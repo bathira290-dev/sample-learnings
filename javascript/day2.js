a993305196c0cf69e54444244ed6016f76b1e422
athira <bathira290@gmail.com>
2026-08-14 12:42:02 +0530

    javascript portion

---
+++ javascript/day2.js
@@ -0,0 +1,40 @@
+const a = 10;
+const b = 20;
+
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** 3); // 10 * 10 * 10
+
+let num = 10;
+// const inc = num++; // post incremenmt
+const inc = ++num; // pre increment
+console.log(inc, num);
+
+let nu = 20;
+ const incr = nu++;// post increment
+ console.log(incr, nu);
+ 
+
+let n = 10;
+const d = --n;
+console.log(n, d);// decre
+
+console.log(5 == 5)
+console.log(5 === "5")
+console.log(5 === "5")
+
+
+if (10 > 20)
+    console.log("This is true statement")
+} else {
+    console.log("This is false statement")
+}
+
+const n1 = 10;
+const n2 = 20;
+const n3 = 30;
+
+ if (n1 > n2 && )
---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+       // let b= 20;
+        // var b= 10;
+        // let b;
+        // var b;
+        const num ="20";
+        // const type = typeof num;
+        console.log(typeof num);
+
+
+        console.log(10 + "10");
+
+        let b;
+        {
+            b= 10;
+        }
+        console.log(b);
+    
